c5a7272d4970c9db4a6132dc4bf345f23abadc63
Nathan Taylor <taylnath@oregonstate.edu>
2021-06-21 00:37:08 -0700

    added files

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, json
+import os
+import database.db_connector as db
+
+# Configuration
+
+app = Flask(__name__)
+#db_connection = db.connect_to_database()
+
+# Routes 
+
+@app.route('/')
+def root():
+    return render_template("main.jinja")
+
+# @app.route('/bsg-people')
+# def bsg_people():
+#     query = "SELECT * FROM bsg_people;"
+#     cursor = db.execute_query(db_connection=db_connection, query=query)
+#     results = cursor.fetchall()
+#     return render_template("bsg.jinja", bsg_people=results)
+
+# Listener
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 9112))
+    app.run(port=port, debug=True)
