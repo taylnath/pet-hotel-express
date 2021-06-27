3218b98864c7872304d2265f5a9a5c01bb4cc7c4
taylnath <taylnath@oregonstate.edu>
2021-06-27 12:39:48 -0700

    removed python stuff

--- python_version/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from routes.test import test as test_route
-
-app = Flask(__name__)
-app.register_blueprint(test_route, url_prefix="/test")
-
-print(app.url_map) # debug
--- python_version/old/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ./venv/bin/activate
-
-gunicorn --bind 0.0.0.0:42001 wsgi:app -D --capture-output --log-file app.log --log-level 'debug'
