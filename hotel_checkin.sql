7d21cd07e44a3108efd814d3e96d6437faa75547
George Pence <penceg@oregonstate.edu>
2021-07-03 16:56:11 -0400

    Added 1 Owner, 2 Queries
    Added two easy queries for possible tabular reports - owner list with pets listed, and owner list with # of pets.
    
    Added an Owner related to another Owner (the Bobertsons both own Gruffy).
    
    So... tabular reports are now weird, with Gruffy listed twice, and counted twice. Hmmm...

--- hotel_checkin.sql
+++ hotel_checkin.sql
@@ -3,6 +3,7 @@
 insert into `Owners` values (1, 'Bob', 'Robertson', 'bob@email.biz');
 insert into `Owners` values (2, 'Mario', 'Robertson', 'mario@email.biz');
 insert into `Owners` values (3, 'Rob', 'Bobertson', 'rob@email.biz');
+insert into `Owners` values (4, 'Alice', 'Bobertson', 'alice@email.biz');
 
 insert into `Employees` values (1, 'Mike', 'Michaels', 'Concierge');
 insert into `Employees` values (2, 'Erik', 'Marcusson', 'Grooming/Pet Care');
@@ -12,7 +13,7 @@ insert into `Employees` values (4, 'Greg', 'Martin', 'Manager');
 insert into `Pets` values (1, 'Fluffy', 'Only likes dogs', 'cat');
 insert into `Pets` values (2, 'Scruffy', 'Only likes cats', 'dog');
 insert into `Pets` values (3, 'Gruffy', 'Likes neither cats nor dogs', 'cat');
-insert into `Pets` values (4, 'Chico', 'wimpy, scared of Black Cats', 'dog');
+insert into `Pets` values (4, 'Chico', 'wimpy, afraid of Black Cats', 'dog');
 
 insert into `Rooms` values (1, 'Our first, and best room');
 insert into `Rooms` values (2, 'Our second room is even better.');
@@ -26,6 +27,7 @@ insert into `Guests` values (1, 1, 1);
 insert into `Guests` values (2, 1, 2);
 insert into `Guests` values (3, 3, 3);
 insert into `Guests` values (4, 1, 4);
+insert into `Guests` values (5, 4, 3);
 
 insert into `Stays` values (1, 1, 1, 1);
 insert into `Stays` values (2, 1, 2, 3);
--- hotel_queries.sql
+++ hotel_queries.sql
@@ -1,8 +1,27 @@
 -- list owners and their pets
 select O.firstName, P.name, P.type from Owners O natural join Guests G natural join Pets P; 
 
--- get all bookings on a given date (TODO: is checkout on the last day?)
-select * from Bookings where '2022-1-12' between startDate and endDate;
+-- get all bookings on a given date (Note that traditionally, end date = checkout date for lodging)
+-- thus, checkout date / end date is available to book.  we'll make that explicit to Owner in web display)
+select * from Bookings where '2022-1-12' between startDate and (endDate - 1);
 
 -- get all rooms booked on a given date (since we don't plan to assign rooms this is not a query we'll use)
 select R.roomID, B.startDate, B.endDate from Bookings B natural join Stays natural join Rooms R;
+
+-- ============================================================================
+-- tabular reports
+-- ============================================================================
+
+-- Owner List with Pet List
+select
+  o.ownerID, o.firstName, o.lastName, p.petID, p.name, p.type, p.preferences from
+  Owners o left join Guests g on o.ownerID = g.ownerID left join
+  Pets p on p.petID = g.petID order by
+  o.lastName;
+
+-- Owner List with count of # of Pets they own
+select o.ownerID, o.firstName, o.lastName, count(p.petID) as '# of Pets' from
+  Owners o left join Guests g on o.ownerID = g.ownerID left join
+  Pets p on p.petID = g.petID group by
+  o.ownerID order by
+  o.lastName, o.firstName asc;
