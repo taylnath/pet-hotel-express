1571bc0ad0edde8a36ba6af2a59a30bae9fcd70d
George (Mac) Pence <71496139+georgepence@users.noreply.github.com>
2021-07-07 17:28:05 -0400

    Home Page Idea
    This could be a minimal but sufficient home page, a placeholder to refine, or a final solution if we don't have time to improve it.
    
    Normally we should devote several nice pages to selling the website and the business.  But we can do that when we are further along.  Or leave it simple, to keep the grader from having to look at pages with no database element.

--- client/src/Routes/Home.js
+++ client/src/Routes/Home.js
@@ -1,6 +1,57 @@
+import { Container, Row, Col, Card, Image } from "react-bootstrap";
+import HeaderImage from '../Components/images/pet_hotel_home_main.jpeg'
+import SereneCat from '../Components/images/serene_cat.jpeg'
+import BlackCat from '../Components/images/black_cat.jpeg'
+import HappyDog from '../Components/images/happy_dog.jpeg'
+
 function Home() {
   return (
-    <h1>Home</h1>
+      <div>
+      <div style={{minHeight: 500, backgroundImage: `url(${HeaderImage})`, backgroundRepeat: "no-repeat",backgroundSize: 'w-100'}}>
+    <h1 style={{fontSize: 60, color: "black", padding: 5}}>Pet Hotel</h1>
+      </div>
+    <Container>
+      <Row style={{padding: 15}}>
+        <Col>
+          Talk about PetHotel
+        </Col>
+        <Col>
+          <Image src={SereneCat} style={{width: 450}} rounded />
+        </Col>
+      </Row>
+  
+      <Row>
+        <Col>
+          <Image src={HappyDog} style={{width: 450}} rounded />
+        </Col>
+        <Col>
+        Talk about Rooms
+        </Col>
+      </Row>
+  
+      <Row>
+        <Col>
+        Talk about Pricing
+        </Col>
+        <Col>
+          <Image src={BlackCat} style={{width: 450}} rounded />
+        </Col>
+      </Row>
+  
+      <Row>
+        <Col>
+          <Card>
+            picture
+          </Card>
+        </Col>
+        <Col>
+        Vaccination Information
+        </Col>
+      </Row>
+      
+    </Container>
+      </div>
+
   );
 }
 
