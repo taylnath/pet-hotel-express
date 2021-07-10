c98a01e02e7430b77c5251e200d1cd241764bc8f
George (Mac) Pence <71496139+georgepence@users.noreply.github.com>
2021-07-10 15:02:26 -0400

    Improved table appearance in Employees

--- client/src/Components/ShowReport.js
+++ client/src/Components/ShowReport.js
@@ -3,9 +3,9 @@ import ReportRows from "./ReportRows";
 function Reports(props) {
 
   return (
-      <div>
+      <>
         <ReportRows report_rows={props.report_rows} attributes={props.attributes} />
-      </div>
+      </>
   );
 }
 
